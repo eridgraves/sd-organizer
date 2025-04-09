e03c7495e52918097462c918231b3134187d211d
Eric Graves <eridgraves@gmail.com>
2025-04-08 22:58:56 -0400

    Add start of byte read over SPI

--- src/sd-driver.c
+++ src/sd-driver.c
@@ -7,9 +7,15 @@
  *  - Write not supported. Write protect switch is not honored.
  *  - Init erase not supported.
  *  
+ *  Resources:
+ *  - https://www.cactus-tech.com/wp-content/uploads/2019/03/An-Introduction-To-SD-Card-Interface.pdf
+ *  - https://github.com/torvalds/linux/blob/master/drivers/mmc/core/sd.c
  */
 
 // #include
+
+// Start at 100% SPI CLK speed
+#define SPI_CLK_SPEED 100 
 
 /// @brief Power on cycle according to SD spec
 ///        Must hold 1ms (74 clock cycles) per spec. 
@@ -54,4 +60,55 @@ int sd_setup_card(struct spi_handle *spi_handle, bool reinit)
 
 
 
+}
+
+/// @brief Bitbang delay -- spin for a bit 
+void spi_delay()
+{
+    volatile int v;   // to avoid optimization
+    for(int i=0; i<SPI_CLK_SPEED/2; i++)
+        v;
+    
+}
+
+/// @brief Set SPI clock polarity (CPOL)
+void spi_set_clk_pol(unsigned char value)
+{
+    // HW stuff
+}
+
+/// @brief Set SPI clock phase (CPHA)
+void spi_set_clk_phase(unsigned char value)
+{
+    // HW stuff
+}
+
+
+unsigned char spi_read_byte()
+{
+    // data on falling edge
+    unsigned char byte_data = 0;
+
+    // negative clock 
+    spi_set_clk_pol(0);
+    spi_set_clk_phase(0);
+
+    // 1 byte of data
+    for (unsigned char b=0; b<8; b++)
+    {
+        // delay by half clock
+        spi_delay(SPI_CLK_SPEED/2);
+
+        // set clk
+
+        // wait
+
+        // set clk
+
+        // data
+
+        // MISO
+    }
+
+    return byte_data;
 }
